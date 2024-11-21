afbd8d40ca5955fb499b94c9bf38df3f93e52de5
Hasibul Hasan <hasibul277127@gmail.com>
2024-11-21 23:47:45 +0600

    a small revision

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
     return (
         <div className="text-center my-10 ">
             <h1 className="mb-5 text-xl font-bold">Welcome to your profile, {user.displayName}</h1>
-            <img className="mx-auto rounded-full w-40 my-10" src={user.photoURL} alt="" />
+            <img className="mx-auto rounded-full w-40 h-40 my-10" src={user.photoURL} alt="" />
             <h2>Email: {user.email}</h2>
             <h2 className="text-base font-semibold mb-5">Name: {user.displayName}</h2>
             <Link className="btn bg-sky-600 text-white" to="/update">UPDATE</Link>
